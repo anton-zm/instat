263502e23b917f98b885da67c3a7ebb0b769f8d3
anton-zm <antonzm34@gmail.com>
2021-08-07 22:10:04 +0300

    add fonts

--- instat/src/components/header.tsx
+++ instat/src/components/header.tsx
@@ -11,11 +11,26 @@ const HeaderWrapper = styled.header`
 const Logo = styled.img`
     width: 60px;
 `
-
+const MenuLink = styled.a`
+    font-size: 12px;
+    text-transform: uppercase;
+    color: white;
+    text-decoration: none;
+    margin-left: 25px;
+    cursor: pointer;
+    opacity: 0.7;
+    transition: 0.3s;
+    :hover {
+        opacity: 1;
+    }
+`
+// Pseudo-menu)))
 const HeaderMenu = () => {   
     return (
-        <nav>
-            
+        <nav className='flex-row'>
+            {['football', 'ice hockey', 'basketball', 'handball', 'instat tv', 'media', 'blog'].map((item) => (
+                <MenuLink key={item}>{item}</MenuLink>
+            ))}
         </nav>
     )
 }
