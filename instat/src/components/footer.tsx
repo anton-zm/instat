5b2431fb6000a8b43c02db34fddf440a277038cd
anton-zm <antonzm34@gmail.com>
2021-08-07 19:10:47 +0300

    logo & init step 2

---
+++ instat/src/components/footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components'
+import { Content } from './content-wrapper'
+import logo from '../assets/img/logo.svg'
+
+
+
+export const Footer = () => { 
+    return ( 
+        <footer> 
+            <span>Footer</span>
+        </footer> 
+    )
+}
+
---
+++ instat/src/components/header.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components'
+import { Content } from './content-wrapper'
+import logo from '../assets/img/logo.svg'
+
+const HeaderWrapper = styled.header`
+    padding: 20px 0;
+    background-color: black;
+    width: 100%;
+`
+const Logo = styled.img`
+    width: 60px;
+`
+
+const HeaderMenu = () => {   
+    return (
+        <nav>
+            
+        </nav>
+    )
+}
+
+export const Header = () => { 
+    return (
+        <HeaderWrapper className='header'>
+            <Content className='flex-row between centered-align'>
+                <Logo src={logo} alt='logo inStat' />
+                <HeaderMenu />
+            </Content>
+        </HeaderWrapper>
+    )
+}
+
